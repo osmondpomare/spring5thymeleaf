3839c0ea47fc2552f57ae45851741500eecf0686
osmpom <osmpom@eltiempo.com>
2019-05-21 11:21:15 -0500

    Base project running

--- src/main/java/co/com/spring/controller/TestController.java
+++ src/main/java/co/com/spring/controller/TestController.java
@@ -1,7 +1,12 @@
 package co.com.spring.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class TestController {
 	
+	@RequestMapping("/test")
 	public String test() {
 		return "test";
 	}
