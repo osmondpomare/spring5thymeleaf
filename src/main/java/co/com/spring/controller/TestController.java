8531fe5e49ddfde547f750dad4fbd0d255f93109
osmpom <osmpom@eltiempo.com>
2019-05-21 14:42:33 -0500

    Ajustes proyecto base

--- src/main/java/co/com/spring/controller/TestController.java
+++ src/main/java/co/com/spring/controller/TestController.java
@@ -1,13 +1,18 @@
 package co.com.spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@RequestMapping("/test")
 	public String test() {
+		logger.info("test controller");
 		return "test";
 	}
 
